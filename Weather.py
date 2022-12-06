ec8d47013cf66e9abf41c42dc39a1c03c47fad11
Souvik Ghosh <114401034+Souvik0216J@users.noreply.github.com>
2022-12-06 20:45:46 +0530

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,35 @@
+import requests 
+from bs4 import BeautifulSoup 
+import os
+# enter city name
+city = str(input("Enter City Name: "))
+
+# creating url and requests instance
+url = f"https://www.google.com/search?q="+"weather"+city
+html = requests.get(url).content
+
+# getting raw data
+soup = BeautifulSoup(html, 'html.parser')
+temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+
+# formatting data
+data = str.split('\n')
+time = data[0]
+sky = data[1]
+
+# getting all div tag
+listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+strd = listdiv[5].text
+
+# getting other required data
+pos = strd.find('Wind')
+other_data = strd[pos:]
+
+# printing all data
+print("Temperature is", temp)
+print("Time: ", time)
+print("Sky Description: ", sky)
+print(other_data)
+
+os.system('pause')
